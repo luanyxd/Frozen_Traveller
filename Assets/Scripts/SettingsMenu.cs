6e9a56235b30d89c50406b9925f5d6aa14ac62db
EveZhou0102 <zhouqi@usc.edu>
2021-02-08 23:11:15 -0800

    Add 2 backgrounds assets, LevelChangers(Fading), update main menu, add control to volume

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMusic (float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+    }
+
+    public void SetSound(float volume)
+    {
+        audioMixer.SetFloat("sound", volume);
+    }
+}
