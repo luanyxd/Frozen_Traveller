4acd8f714c619d90d8cac1b5e36afa35ce82af7c
cpeng515 <70790389+cpeng515@users.noreply.github.com>
2021-02-16 18:19:04 -0800

    add fireball

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+
+    {
+        Debug.Log(other.gameObject.CompareTag("Player"));
+
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            rb.velocity = new Vector2(-2, -10);
+            rb.isKinematic = false;
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            //Destroy(gameObject);
+        }
+    }
+}
