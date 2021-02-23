161dbd6d7662be108f7481bc630c4cd5d1095e66
cpeng515 <70790389+cpeng515@users.noreply.github.com>
2021-02-22 23:37:27 -0800

    add fireballs, rocks and fix camera

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            rb.isKinematic = false;
+            rb.velocity = new Vector2(-3, 0);
+        }
+    }
+}
