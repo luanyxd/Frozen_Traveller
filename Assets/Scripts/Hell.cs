ebfb8b85c3124644cbc9bc1eb61ca41942ba5240
zjj-shiyi <70492796+zjj-shiyi@users.noreply.github.com>
2021-03-04 19:56:14 -0800

    Update death of falling

---
+++ Assets/Scripts/Hell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hell : MonoBehaviour
+{
+    //public int damage;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameManager.CompleteLevel(true);
+        }
+    }
+}
