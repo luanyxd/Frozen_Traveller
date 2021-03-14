3f05f77aba01c1da9aeb241db5ea00881a3c038a
zichuanf <zichuanf@usc.edu>
2021-03-14 12:16:28 -0700

    Add support of BGM slider

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -41,4 +41,15 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Stop();
     }
+
+    public void SetVolume(string name, float vol)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.volume = vol;
+        s.source.volume = vol;
+    }
+    public void SetThemeVolume(float vol)
+    {
+        SetVolume("Theme1", vol);
+    }
 }
