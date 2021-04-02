7b1cf007a72a09b64e4779d14df95b529d88b6fd
zjj-shiyi <70492796+zjj-shiyi@users.noreply.github.com>
2021-04-01 22:56:15 -0700

    Update prototype

---
+++ Assets/Scripts/FallingItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public int Speed;
+    private PlayerHealth playerHealth;
+    public int damage;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            rb.velocity = new Vector2(0, 0-Speed);
+            rb.isKinematic = false;
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rb.isKinematic = true;
+            playerHealth.DamagePlayer(damage);
+        }
+        rb.velocity = new Vector2(0, 0);
+        Destroy(gameObject);
+    }
+    
+}
