0d087762cefd0c477ac923eba0f2846f9ff89f3e
zichuanf <zichuanf@usc.edu>
2021-04-14 11:24:57 -0700

    Added achievements for level3

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -18,13 +18,14 @@ public class CollectableItem : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Debug.Log("Collided.");
-        //Debug.Log(other.gameObject.tag);
-        //Debug.Log(other.GetType().ToString());
+        Debug.Log("Collided.");
+        Debug.Log(other.gameObject.tag);
+        Debug.Log(other.GetType().ToString());
         if (other.gameObject.CompareTag("Player") && other.GetType().ToString()== "UnityEngine.CapsuleCollider2D")
         {
             FindObjectOfType<GlobalAchieve>().TriggerAchievementById(achievementId);
             FindObjectOfType<AudioManager>().Play("Coin");
+            Debug.Log("Destroying.");
             Destroy(gameObject);
         }
     }
