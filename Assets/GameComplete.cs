579e4de0b216a40b6faf633c37e4853cf9eae6fc
EveZhou0102 <zhouqi@usc.edu>
2021-04-22 16:31:39 -0700

    Update Final Title

--- Assets/GameComplete.cs
+++ Assets/GameComplete.cs
@@ -9,6 +9,9 @@ public class GameComplete : MonoBehaviour
     public TMP_Text timeAmount;
     public TMP_Text coinAmount;
     public TMP_Text historyHighest;
+    public GameObject student;
+    public GameObject scholar;
+    public GameObject environmentalist;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,24 @@ public class GameComplete : MonoBehaviour
     {
         timeAmount.text = PlayerPrefs.GetFloat("final_time").ToString("F2") + "s";
         coinAmount.text = PlayerPrefs.GetInt("final_score").ToString();
+
+        if (PlayerPrefs.GetInt("final_score") < 100)
+        {
+            student.SetActive(true);
+            scholar.SetActive(false);
+            environmentalist.SetActive(false);
+        } else if (PlayerPrefs.GetInt("final_score") < 200)
+        {
+            student.SetActive(false);
+            scholar.SetActive(true);
+            environmentalist.SetActive(false);
+        } else
+        {
+            student.SetActive(false);
+            scholar.SetActive(false);
+            environmentalist.SetActive(true);
+        }
+
         if (PlayerPrefs.GetFloat("shortest_time", -1f) == -1f)
             historyHighest.text = "9999s / 0";
         else
